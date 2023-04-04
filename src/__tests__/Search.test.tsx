4050c9c74e3401708713677fc57d523fdf3f8712
Ilya Vylegzhanin <vylegzhanin.ilja@yandex.ru>
2023-04-04 12:43:31 +0300

    still crash

---
+++ src/__tests__/Search.test.tsx
@@ -0,0 +1,27 @@
+import { fireEvent, render, screen } from '@testing-library/react';
+import Search from '../components/search/Search';
+
+describe('Search', () => {
+  let search: HTMLInputElement;
+  let searchBtn: HTMLButtonElement;
+  beforeEach(() => {
+    render(<Search handleSearchBtn={() => {}} />);
+    search = screen.getByPlaceholderText('Search...');
+    searchBtn = screen.getByRole('button');
+  });
+
+  it('should render search elem', async () => {
+    expect(search).toBeInTheDocument();
+  });
+
+  it('should change value inside', async () => {
+    fireEvent.change(search, { target: { value: 'iphone 9' } });
+    expect((search as HTMLInputElement).value).toBe('iphone 9');
+  });
+
+  it('should not default submit', async () => {
+    fireEvent.change(search, { target: { value: 'iphone 9' } });
+    fireEvent.click(searchBtn);
+    expect((search as HTMLInputElement).value).toBe('iphone 9');
+  });
+});
