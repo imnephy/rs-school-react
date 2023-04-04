9aa2d5e0bccd93effd0bb4044fdb2341f2110603
Ilya Vylegzhanin <vylegzhanin.ilja@yandex.ru>
2023-04-04 12:42:37 +0300

    crash

--- src/__tests__/ProductService.test.tsx
+++
@@ -1,8 +0,0 @@
-import ProductService from '../API/ProductService';
-
-describe('Product service', () => {
-  it('should return data', async () => {
-    const data = await ProductService.getAll();
-    expect(data.products.length).toBe(3);
-  });
-});
